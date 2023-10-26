da0a8ac208897bdd38f0e2ce7081b63607c49288
ArtemMatyushechkin <73058755+ArtemMatyushechkin@users.noreply.github.com>
2023-10-26 09:48:02 +0400

    Initial commit

---
+++ UniversalInventorySystem-master/Assets/test/ItemObjcet.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Item")]
+public class ItemObjcet : ScriptableObject
+{
+    public new string name;
+    public string description;
+    public Sprite icon;
+    public int cost;
+   
+    public ItemTypes selectItemType = new ItemTypes();
+    public enum ItemTypes
+    {
+        Sword,
+        Axe,
+        Ranged,
+        Resource,
+        Usable
+     
+    }
+}
